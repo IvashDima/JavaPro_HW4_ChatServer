a76897cf6749055668d5a6b9a79c673f625b259c
Dmytro Ivashchenko <dnytsyk@gmail.com>
2025-03-16 23:47:46 +0100

    Added inactive timer & checkInactiveUsers.

--- src/main/java/academy/prog/servlets/GetStatusServlet.java
+++ src/main/java/academy/prog/servlets/GetStatusServlet.java
@@ -1,37 +1,35 @@
 package academy.prog.servlets;
 
 import academy.prog.enums.StatusType;
-import academy.prog.models.MessageList;
 import academy.prog.models.UserList;
 import jakarta.servlet.http.HttpServlet;
 import jakarta.servlet.http.HttpServletRequest;
 import jakarta.servlet.http.HttpServletResponse;
 
 import java.io.IOException;
-import java.io.OutputStream;
 import java.io.PrintWriter;
-import java.nio.charset.StandardCharsets;
+import java.util.Map;
 import java.util.Objects;
+import java.util.concurrent.ConcurrentHashMap;
 
 public class GetStatusServlet extends HttpServlet {
-    //	private static final long inactiveTime = 30_000;
+    private static final long inactiveTime = 30000;
     private UserList usrList = UserList.getInstance();
 
-    //	private final Gson gson = new Gson();
-//	private final Map<String, Long> lastRequestTime = new ConcurrentHashMap<>();
+	private final Map<String, Long> lastRequestTime = new ConcurrentHashMap<>();
 
-//	public GetListServlet() {
-//		new Thread(() -> {
-//			while (true) {
-//				try {
-//					Thread.sleep(10_000); //
-//					checkInactiveUsers();
-//				} catch (InterruptedException e) {
-//					e.printStackTrace();
-//				}
-//			}
-//		}).start();
-//	}
+	public GetStatusServlet() {
+		new Thread(() -> {
+			while (true) {
+				try {
+					Thread.sleep(30000);
+					checkInactiveUsers();
+				} catch (InterruptedException e) {
+					e.printStackTrace();
+				}
+			}
+		}).start();
+	}
 
     @Override
     protected void doGet(HttpServletRequest req, HttpServletResponse resp) throws IOException {
@@ -40,7 +38,7 @@ public class GetStatusServlet extends HttpServlet {
             try {
                 if(login != null){
                     usrList.setUserActive(login);
-//                    lastRequestTime.put(login, System.currentTimeMillis());
+                    lastRequestTime.put(login, System.currentTimeMillis());
                 }
             } catch (Exception ex) {
                 resp.setStatus(HttpServletResponse.SC_BAD_REQUEST);
@@ -62,16 +60,16 @@ public class GetStatusServlet extends HttpServlet {
             resp.getWriter().write("Internal Server Error: " + ex.getMessage());
         }
     }
-    //	private void checkInactiveUsers() {
-//		long currentTime = System.currentTimeMillis();
-//		for (Map.Entry<String, Long> entry : lastRequestTime.entrySet()) {
-//			String login = entry.getKey();
-//			long lastTime = entry.getValue();
-//			if (currentTime - lastTime > inactiveTime) {
-//				usrList.setUserInactive(login);
-//				lastRequestTime.remove(login);
-//				System.out.println("User " + login + " set to inactive status.");
-//			}
-//		}
-//	}
+    	private void checkInactiveUsers() {
+		long currentTime = System.currentTimeMillis();
+		for (Map.Entry<String, Long> entry : lastRequestTime.entrySet()) {
+			String login = entry.getKey();
+			long lastTime = entry.getValue();
+			if (currentTime - lastTime > inactiveTime) {
+				usrList.setUserInactive(login);
+				lastRequestTime.remove(login);
+				System.out.println("User '" + login + "' set to inactive status.");
+			}
+		}
+	}
 }
